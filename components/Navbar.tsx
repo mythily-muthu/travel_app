56b929a9fae521309352f70ca0e8b94fe2a95a23
mythily-muthu <mythilimuthu18@gmail.com>
2023-10-27 17:19:37 +0530

    Feat: added public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="py-5 border-2 border-red-400 justify-between relative z-30 ">
-      <Link href="/">home</Link>
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
     </nav>
   );
 };
