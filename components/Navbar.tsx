b138c12854837d0e2a240f9182ebf67f4a9428f8
mythily-muthu <mythilimuthu18@gmail.com>
2023-10-29 18:37:45 +0530

    Feat: Navbar is designs developed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,40 @@
+import { NAV_LINKS } from "@/app/constants";
 import Image from "next/image";
 import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="py-5 border-2 border-red-400 justify-between relative z-30 ">
+    <nav className="py-5 flexBetween relative z-30 flex padding-container ">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
+      <ul className="hidden lg:flex h-full gap-12">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold "
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="hidden lg:flexCenter ">
+        <Button
+          type="button"
+          icon="/user.svg"
+          title="Login"
+          variant="btn_dark_green"
+        />
+      </div>
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
